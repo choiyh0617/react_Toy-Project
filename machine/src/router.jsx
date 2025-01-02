d127fc9be5610d846320887b3ab57c697307a99d
choiyh0617 <6l0mrc00057@gmail.com>
2025-01-03 02:34:45 +0900

    .

---
+++ machine/src/router.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router';
+
+// const router = () => {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path='/' element = {}/>
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// };
+
+// export default router;
